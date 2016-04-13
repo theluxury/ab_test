1260853112faf23e3180682f2da9138f9e2c7e47
Mark Wang <zhengkaw@gmail.com>
2016-04-13 16:23:38 -0700

    Added some documentations and some checks for adding/removing tags

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,19 +7,44 @@ abTestApp.controller('ABFieldsCtrl', function ($scope, $http) {
 	$scope.fields = data;
     });
 
+    var _TAG_KINDS = ['Treatment Tags', 'Success Tags'];
+
     var findObject = function(key) {
-	var objectArray = $scope.fields.filter(function( obj ) {
+	var objectArray = $scope.fields.filter(function(obj) {
 	    return obj.key == key;
 	});
 	return objectArray[0];
     }
-    
+
+    /**
+     * Adds a new field to put information for tags
+     * @param {String} key, the kind of tag you want to push. Must be either 'Treatment Tags' or 'Success Tags' 
+     */
     $scope.addNewTag = function(key) {
+	if (_TAG_KINDS.indexOf(key) < 0) {
+	    throw "Trying to push tag onto key that is not Treatment Tags or Success Tags";
+	    return;
+	}
 	var jsonObject = findObject(key);
 	jsonObject.tags.push({});
     };
+    /**
+     * Removes last field to put information for tags
+     * @param {String} key, the kind of tag you want to push. Must be either 'Treatment' or 'Success' 
+     */
     $scope.removeLastTag = function(key) {
+	if (_TAG_KINDS.indexOf(key) < 0) {
+	    throw "Trying to push tag onto key that is not Treatment Tags or Success Tags";
+	    return;
+	}
 	var jsonObject = findObject(key);
 	jsonObject.tags.pop();
+    }
+
+    /**
+     * Submits the form to mongo db.
+     */
+    $scope.submitForm = function() {
+	console.log($scope.fields);
     }
 });
