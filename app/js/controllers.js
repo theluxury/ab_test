df374d2793f7c07d5421cb9d756e8eb2a5b5bddf
Mark Wang <zhengkaw@gmail.com>
2016-04-13 15:36:27 -0700

    Moves fields to own file

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,22 +2,18 @@
 /* Controllers */
 var abTestApp = angular.module('abTestApp', []);
 
-abTestApp.controller('ABFieldsCtrl', function ($scope) {
-    $scope.fields = [
-	{'key': 'Test Name', 'type': 'text'},
-	{'key': 'Description', 'type': 'block'},
-	{'key': 'Start Date', 'type': 'date'},
-	{'key': 'End Date', 'type': 'date'},
-	{'key': 'Comment', 'type': 'block'},
-	{'key': 'Category', 'type': 'select', 'options': ['Frontend', 'Backend', 'Product', 'Design']},
-	{'key': 'Product', 'type': 'select', 'options': ['CRM', 'Inbox', 'IQCloud']},
-	{'key': 'Owner', 'type': 'text'},
-	{'key': 'Email', 'type': 'email'},
-	// TODO: is rules the right word here?
-	{'key': 'Treatments Tags', 'type': 'object', 'tags': [{}]},
-	{'key': 'Success Tags', 'type': 'object', 'tags': [{}]}
-    ];
+abTestApp.controller('ABFieldsCtrl', function ($scope, $http) {
+    $http.get('fields/fields.json').success(function(data) {
+	$scope.fields = data;
+    });
 
+    var findObject = function(key) {
+	var objectArray = $scope.fields.filter(function( obj ) {
+	    return obj.key == key;
+	});
+	return objectArray[0];
+    }
+    
     $scope.addNewTag = function(key) {
 	var jsonObject = findObject(key);
 	jsonObject.tags.push({});
@@ -26,13 +22,4 @@ abTestApp.controller('ABFieldsCtrl', function ($scope) {
 	var jsonObject = findObject(key);
 	jsonObject.tags.pop();
     }
-
-    var findObject = function(key) {
-	var objectArray = $scope.fields.filter(function( obj ) {
-	    return obj.key == key;
-	});
-	return objectArray[0];
-    }
-	
-    
 });
